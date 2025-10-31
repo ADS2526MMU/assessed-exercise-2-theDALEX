7cead3155d40e35880bce064917c5a4e8dd59a26
Dalex Davis <thedalexdavis@gmail.com>
2025-10-31 14:27:02 +0000

    Implemented InOrder, PreOrder and PostOrder for BinTree

--- ADSPortEx2/BinTree.cs
+++ ADSPortEx2/BinTree.cs
@@ -28,21 +28,50 @@ namespace ADSPortEx2
 
 
         //Functions for EX.2A
+        //InOrder Left -> Root -> Right
         public void InOrder(ref string buffer)
         {
-            throw new NotImplementedException();
+            inOrder(root, ref buffer);
+        }
+        private void inOrder(Node<T> tree, ref string buffer)
+        {
+            if (tree != null)
+            {
+                inOrder(tree.Left, ref buffer);
+                buffer += tree.Data.ToString() + ",";
+                inOrder(tree.Right, ref buffer);
+            }
         }
 
+        //PreOrder Root -> Left -> Right
         public void PreOrder(ref string buffer)
         {
-            throw new NotImplementedException();
+            preOrder(root, ref buffer);
+        }
+        private void preOrder(Node<T> tree, ref string buffer)
+        {
+            if (tree != null)
+            {
+                buffer += tree.Data.ToString() + ",";
+                preOrder(tree.Left, ref buffer);
+                preOrder(tree.Right, ref buffer);
+            }
         }
 
+        //PostOrder Left -> Right ->Root
         public void PostOrder(ref string buffer)
         {
-            throw new NotImplementedException();
+            postOrder(root, ref buffer);
         }
-
+        private void postOrder(Node<T> tree, ref string buffer)
+        {
+            if (tree != null)
+            {
+                postOrder(tree.Left, ref buffer);
+                postOrder(tree.Right, ref buffer);
+                buffer += tree.Data.ToString() + ",";
+            }
+        }
         //Free space, use as necessary to address task requirements... 
 
 
