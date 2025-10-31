818e9eb6c076bccc3b2d606ba4c00c6cc1779494
Dalex Davis <thedalexdavis@gmail.com>
2025-10-31 11:59:59 +0000

    a simple menu driven test interface is added to test the system

--- ADSPortEx2/Program.cs
+++ ADSPortEx2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,9 +21,42 @@ namespace ADSPortEx2
             // "2. Display all items... ect
             //}
 
+            Console.WriteLine("=== Retro gaming preservation project system ===");
 
+            bool running = true;
 
-
+            while (running)
+            {
+                Console.WriteLine("\nSelect an option:");
+                Console.WriteLine("1. Add Video Game");
+                Console.WriteLine("2. Display all Video Games in InOrder");
+                Console.WriteLine("3. Display all Video Games in PreOrder");
+                Console.WriteLine("4. Display all Video Games in PostOrder");
+                Console.WriteLine("5. Display Video Game with earliest release year");
+                Console.WriteLine("6. Current Height of the tree");
+                Console.WriteLine("7. Exit");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        // Code to add a video game
+                        Console.WriteLine("Adding a new video game...");
+                        // Implementation goes here
+                        break;
+                    case "2":
+                        // Code to display all video games
+                        Console.WriteLine("Displaying all video games...");
+                        // Implementation goes here
+                        break;
+                    case "7":
+                        running = false;
+                        Console.WriteLine("Exiting the system. Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
             Console.ReadLine();
 
         }
